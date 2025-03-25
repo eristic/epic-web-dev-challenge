f92294fbc2dd77bc9e9047a11a4712b904cb0c5f
Adam Fortuna <adam@fortuna.name>
2025-03-25 11:05:37 -0600

    Updated libs

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import MuxPlayer from "@mux/mux-player-react";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="w-full h-full grid grid-cols-2">
+      <div>
+        <p>Controls here</p>
+      </div>
+      <div>
       <MuxPlayer
-        playbackId="C00OH5HO3jiNjENMkT1lqxoaT4hlAmQhV5JiR010101uX9g"
-        metadata={{
-          video_id: "video-id-54321",
-          video_title: "Birds, birds, burds",
-          viewer_user_id: "user-id-007",
-        }}
-      />
+          playbackId="C00OH5HO3jiNjENMkT1lqxoaT4hlAmQhV5JiR010101uX9g"
+          accentColor="blue"
+          metadata={{
+            video_id: "video-id-54321",
+            video_title: "Birds, birds, burds",
+            viewer_user_id: "user-id-007",
+          }}
+        />
+      </div>
     </div>
   );
 }
