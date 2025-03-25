1bdd4304b1f2dcf307797fcf74a43fd907ed4f77
Scott  Stewart <scottstewart@Scotts-Laptop.ad.canarchy.beer>
2025-03-25 12:58:50 -0600

    Rebase

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { SeekBackwards } from "./player/SeekBackwards";
 import { SeekForwards } from "./player/SeekForwards";
 import { TimeDisplay } from "./player/TimeDisplay";
 import './styles/animations.css';
+import './app.css';
+import clouds from './assets/cloud.svg';
 
 function App() {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -18,12 +20,14 @@ function App() {
   }
 
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-blue-200">
+    <div className="w-full h-screen flex justify-center items-center bg-blue-200 relative">
+      
       <button className="absolute top-0 left-0 border" onClick={togglePlay}>Test</button>
       
-        <MediaController className="relative w-full h-screen bg-blue-200">
+        <MediaController className="absolute inset-0 flex flex-col justify-center items-center bg-blue-200">
+        <img className="clouds" src={clouds} alt="clouds" />
           <MuxVideo ref={videoRef} playbackId="C00OH5HO3jiNjENMkT1lqxoaT4hlAmQhV5JiR010101uX9g" slot="media" muted
-            className="self-center w-64 h-32"
+            className="content-center w-[600px] h-[400px]"
           />
           <PlayButton />
           <SeekBackwards />
